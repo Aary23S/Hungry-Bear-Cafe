bd09c3905b69118dd2560e71f73162af249fa9f3
unknown <SAary>
2024-11-27 00:43:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+navToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+
+  hamburger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+  });
+});
